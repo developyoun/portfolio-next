9f20994d5a0c98c92e8fa0359ecf6e26f70ae362
pyoun <59079426+developyoun@users.noreply.github.com>
2021-06-15 16:11:46 +0900

    my first commit

---
+++ components/Header.tsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+
+import styled, { keyframes } from "styled-components";
+import { AiOutlineMail, AiOutlinePhone, AiOutlineFacebook } from "react-icons/ai";
+
+type FontType = {
+  fontsize?: string;
+};
+
+const HoverInAnimation = keyframes`
+  from{ color: rgb(176, 190, 197) }
+  to{ color: rgb(176, 190, 197, 0.4) }
+`;
+const HoverOutAnimation = keyframes`
+  from{ color: rgb(176, 190, 197, 0.4) }
+  to{ color: rgb(176, 190, 197) }
+`;
+const Vibrate = keyframes`
+  0%{
+    transform: translateX(0);
+  }
+  25%{
+    transform: translateX(-0.2rem);
+  }
+  50%{
+    transform: translateX(0);
+  }
+  75%{
+    transform: translateX(0.2rem);
+  }
+  100%{
+    transform: translateX(0)
+  }
+`;
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 2rem;
+  padding: 1rem 2rem;
+  background-color: #222;
+  z-index: 1;
+`;
+const Div = styled.div`
+  a {
+    text-decoration: none;
+    :link, :visited {
+      animation: ${HoverOutAnimation} 0.5s ease-in alternate forwards;
+    }
+    :hover {
+      animation: ${HoverInAnimation} 0.5s ease-in-out alternate forwards;
+    }
+  };
+  svg{
+    padding-left: 2rem;
+    font-size: 2.6rem;
+    color :rgb(176, 190, 197);
+    :hover{
+      cursor: pointer;
+      animation: ${Vibrate} 0.2s linear infinite;
+    }
+  }
+`;
+const Font = styled.div<FontType>`
+  font-size: ${props => props.fontsize || "2rem"};
+  text-align: center;
+  font-weight: bold;
+`;
+
+const Header = () => {
+  return (
+    <Container>
+      <Div>
+        <Link href="/">
+          <a>
+            <Font fontsize="1.8rem">JaeHo Pyoun</Font>
+            <Font fontsize="0.4rem">byname. developyoun</Font>
+          </a>
+        </Link>
+      </Div>
+      <Div>
+        <AiOutlineMail />
+        <AiOutlinePhone />
+        <AiOutlineFacebook />
+      </Div>
+    </Container>
+  );
+};
+export default Header;
