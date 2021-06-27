6607136a284ba8e0f763f1de1d88654c9ab9aba0
pyoun <59079426+developyoun@users.noreply.github.com>
2021-06-28 01:59:21 +0900

    finish

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,41 @@
 import SubHeader from "../../components/SubHeader";
 
+import styled, { keyframes } from "styled-components";
+
+const Animate = keyframes`
+  0%{
+    transform: translateX(0);
+  }
+  25%{
+    transform: translateX(-2px);
+  }
+  50%{
+    transform: translateX(0px);
+  }
+  75%{
+    transform: translateX(2px);
+  }
+  100%{
+    transform: translateX(0);
+  }
+`;
+
+const Title = styled.div`
+  margin: 4rem;
+  font-size: 3rem;
+  font-weight: bold;
+  text-align: center;
+
+  animation: ${Animate} 0.5s linear infinite;
+`;
+
 const Projects = () => {
   return(
     <>
       <SubHeader />
+      <Title>
+        😁 Project 타이틀을 클릭 해주세요 😀
+      </Title>
     </>
 
   )
