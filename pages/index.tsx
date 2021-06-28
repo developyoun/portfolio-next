f30df1fd8e55be16d82cb829092a274d8c23af7f
pyoun <59079426+developyoun@users.noreply.github.com>
2021-06-28 14:33:54 +0900

    finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import ReactCardFlip from 'react-card-flip';
+import ReactCardFlip from "react-card-flip";
 
 import ProfileImage from "../components/home/ProfileImage";
 import ProfileIntro from "../components/home/ProfileIntro";
 
 import styled from "styled-components";
-import { useState } from 'react';
-import ArrowMark from '../components/home/ArrowMark';
+import { useState } from "react";
+import ArrowMark from "../components/home/ArrowMark";
 
 const Container = styled.div`
   margin: 2rem;
@@ -13,25 +13,22 @@ const Container = styled.div`
 
 export type PropsType = {
   flip: () => void;
-}
+};
 
 const Home = () => {
   const [isFilp, setIsFlip] = useState(false);
-  const handleFlip = () => setIsFlip(!isFilp)
+  const handleFlip = () => setIsFlip(!isFilp);
 
-  return(
+  return (
     <Container>
       <div>
-        <ReactCardFlip 
-          isFlipped={isFilp}
-          >
-          <ProfileImage flip={handleFlip}/>
-          <ProfileIntro flip={handleFlip}/>
+        <ReactCardFlip isFlipped={isFilp}>
+          <ProfileImage flip={handleFlip} />
+          <ProfileIntro flip={handleFlip} />
         </ReactCardFlip>
         <ArrowMark />
       </div>
-      
     </Container>
-  )
+  );
 };
 export default Home;
